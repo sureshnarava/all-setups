8db393daa92f70613af299d0c2447e4c6082078e
sureshnarava <sureshbabunarava@gmail.com>
2025-03-31 10:49:47 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -2,6 +2,10 @@
 #export PATH=$PATH:/usr/local/bin/
 #source .bashrc
 
+#install aws cli 
+curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+unzip awscli-bundle.zip
+sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
 
 #! /bin/bash
 aws configure
@@ -11,8 +15,9 @@ chmod +x kops-linux-amd64 kubectl
 mv kubectl /usr/local/bin/kubectl
 mv kops-linux-amd64 /usr/local/bin/kops
 
-aws s3api create-bucket --bucket cloudanddevopsbyraham0073456.k8s.local --region us-east-1
-aws s3api put-bucket-versioning --bucket cloudanddevopsbyraham0073456.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
-export KOPS_STATE_STORE=s3://cloudanddevopsbyraham0073456.k8s.local
-kops create cluster --name rahams.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.medium
-kops update cluster --name rahams.k8s.local --yes --admin
+aws s3api create-bucket --bucket suresh0402.k8s.local --region us-east-1
+aws s3api put-bucket-versioning --bucket suresh0402.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
+export KOPS_STATE_STORE=s3://suresh0402.k8s.local
+Kops create cluster --name suresh.k8s.local --zones us-east-1a,us-east-1b --master-count 1 --master-size t2.medium --master-volume-size 40 --node-count 2 --node-size t2.micro --node-volume-size 20
+#kops create cluster --name suresh.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.medium
+kops update cluster --name suresh.k8s.local --yes --admin

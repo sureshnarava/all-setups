5a84b19018d6f6b40b2667346ab4fb40f7e61ac4
sureshnarava <sureshbabunarava@gmail.com>
2025-04-02 11:52:27 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -18,6 +18,6 @@ mv kops-linux-amd64 /usr/local/bin/kops
 aws s3api create-bucket --bucket suresh0402.k8s.local --region us-east-1
 aws s3api put-bucket-versioning --bucket suresh0402.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
 export KOPS_STATE_STORE=s3://suresh0402.k8s.local
-kops create cluster --name suresh.k8s.local --zones us-east-1a,us-east-1b --master-count 1 --master-size t2.medium --master-volume-size 40 --node-count 2 --node-size t2.micro --node-volume-size 20
+kops create cluster --name suresh.k8s.local --zones us-east-1a,us-east-1b --master-count 1 --master-size t2.medium --master-volume-size 30 --node-count 2 --node-size t2.micro --node-volume-size 20
 #kops create cluster --name suresh.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.medium
 kops update cluster --name suresh.k8s.local --yes --admin
